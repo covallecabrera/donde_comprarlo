aa82a871271af69d4b4cd50ba6bc70d2f16e6918
covallecabrera <franciscoovalle@192.168.0.5>
2015-05-14 20:42:33 -0400

    Busqueda Categoria v0.2

---
+++ mostrar_marcas_categoria.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * Following code will list all the products
+ */
+
+// array for JSON response
+  $response = array();
+
+
+// include db connect class
+require_once 'db_connect.php';
+
+
+// connecting to db
+ $db = new DB_CONNECT();
+
+$buscar=$_GET["buscar"];
+  
+
+// get all products from products table
+$result = mysql_query("SELECT marca.id_marca, marca.nombre_marca FROM categoria "
+        . " INNER JOIN productos  on categoria.id_categoria=productos.categoria_id_categoria"
+        . " INNER JOIN marca  on marca.id_marca=productos.marca_id_marca"
+        . " WHERE categoria.id_categoria = ".$buscar." ") or die(mysql_error());
+/*
+ SELECT m.nombre_marca FROM categoria c 
+INNER JOIN productos p on c.id_categoria = p.categoria_id_categoria
+INNER JOIN marca m on m.id_marca=p.marca_id_marca
+WHERE c.id_categoria = 2
+ */
+
+// check for empty result
+if (mysql_num_rows($result) > 0) {
+    // looping through all results
+    // products node
+    $response["marca"] = array();
+
+    while ($row = mysql_fetch_array($result)) {
+        // temp user array
+        $marca = array();
+        $marca["id_marca"] = $row["id_marca"];
+        $marca["nombre_marca"] = $row["nombre_marca"];
+
+        //$product["created_at"] = $row["created_at"];
+        //$product["updated_at"] = $row["updated_at"];
+
+
+
+        // push single product into final response array
+        array_push($response["marca"], $marca);
+    }
+    // success
+    $response["success"] = 1;
+
+    // echoing JSON response
+    echo json_encode($response);
+} else {
+    // no products found
+    $response["success"] = 0;
+    $response["message"] = "No se encontraron Marcas";
+
+    // echo no users JSON
+    echo json_encode($response);  
+}
+?>
